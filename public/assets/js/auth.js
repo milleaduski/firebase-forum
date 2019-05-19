e9daa0c4880797ad087f69627f12e3bb5746820d
millea <millea1994@gmail.com>
2019-05-19 14:06:13 +0700

    login with facebook account

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -1,12 +1,24 @@
 var auth = firebase.auth();
 
-function login() {
-	var provider = new firebase.auth.GoogleAuthProvider();
+function login(provider) {
+	switch (provider) {
+		case 'gmail':
+			var provider = new firebase.auth.GoogleAuthProvider(); break;
+		case 'fb':
+			var provider = new firebase.auth.FacebookAuthProvider(); break;
+		default:
+	}
 
 	firebase.auth().signInWithPopup(provider).then(function(result) {
-
+		console.log(result)
 	  alert("Successfully logged in..");
 	}).catch(function(error) {
 	  console.log(error)
+	});
+}
+
+function logout() {
+	auth.signOut().then(function() {
+		alert("Successfully logged out..");
 	});
 }
