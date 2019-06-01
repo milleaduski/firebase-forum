144318a4994174918829848002f5ed4af524fd9b
millea <millea1994@gmail.com>
2019-06-01 14:04:19 +0700

    Add firebase admin

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
-
 const express = require('express');
-
 const cons = require('consolidate');
 
+
+// Require firebase-admin
+const admin = require('firebase-admin');
+
+var serviceAccount = require("private-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://forum-firebase-b3205.firebaseio.com"
+});
+// end setting
+
+const db = admin.firestore();
 const app = express();
 
 app.engine('hbs', cons.handlebars);
@@ -11,6 +22,15 @@ app.set('view engine', 'hbs');
 app.set('views', './views');
 
 app.get('/forum', function(req, res) {
+
+	db.collection('forums').orderBy('updated_at', 'desc').get()
+	.then(snapshot => {
+		snapshot.forEach(doc => {
+			forums.push(doc.data())
+		})
+	}).catch(err => {
+		console.log(err)
+	})
 	res.render('home')
 });
 exports.app = functions.https.onRequest(app);
--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -37,3 +37,33 @@ function logout() {
 		alert("Successfully logged out..");
 	});
 }
+
+var currentUser = null;
+auth.onAuthStateChanged(function(user){
+	if (user) {
+		currentUser = user
+		console.log(currentUser)
+	}
+});
+
+function addTopic() {
+	var title  = document.getElementById('title').value
+	var desc = document.getElementById('desc').value
+	var slug = title.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g,'-')
+
+	db.collection('forums').add({
+		title		: title,
+		slug		: slug,
+		desc		: desc.replace("<br />", "\n"),
+		created_at 	: new Date(),
+		updated_at 	: new Date(),
+		user : {
+			user_id : currentUser.uid,
+			username : currentUser.displayName
+		}
+	}).then(function(docRef){
+		alert("The forum was successfully created")
+	}).catch(function(err){
+		alert("There was an error adding the forum")
+	})
+}
