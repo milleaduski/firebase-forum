294004192995262b346207a4a93d321df61bfdaf
millea <millea1994@gmail.com>
2019-06-02 14:57:43 +0700

    Fetch user data and rendered in template

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,17 @@ const cons = require('consolidate');
 // Require firebase-admin
 const admin = require('firebase-admin');
 
-var serviceAccount = require("private-key.json");
+var serviceAccount = require("./private-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://forum-firebase-b3205.firebaseio.com"
 });
+
+const firestore = admin.firestore();
+
+// Add this magical line of code:
+firestore.settings({ timestampsInSnapshots: true });
 // end setting
 
 const db = admin.firestore();
@@ -22,15 +27,16 @@ app.set('view engine', 'hbs');
 app.set('views', './views');
 
 app.get('/forum', function(req, res) {
-
+var forums = [];
 	db.collection('forums').orderBy('updated_at', 'desc').get()
 	.then(snapshot => {
 		snapshot.forEach(doc => {
+			console.log(doc.data())
 			forums.push(doc.data())
 		})
+		res.render('home', {dataForums:forums})
 	}).catch(err => {
 		console.log(err)
 	})
-	res.render('home')
 });
 exports.app = functions.https.onRequest(app);
